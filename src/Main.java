cc99f5713c63b49e48e6a294de3061e3c56bffdb
sriharithavaka <sriharithavaka0401@gmail.com>
2024-10-08 19:10:00 -0700

    feat: linkingrepo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.HashSet;
+//importing the utility function HashSet
+class DuplicateCheckers{
+    public static boolean hasDuplicates(int[] arr){
+        //hasduplicates is a method where we give integers as input and gets boolean as output
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : arr){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
         }
+        return false;
+    }
+    public static void main(String[] args){
+        int[] arr1 = {1,2,3,2,4,5,1};
+        int[] arr2 = {1,2,3,4};
+        System.out.println("Array1 has duplicates:"+hasDuplicates(arr1));
+        System.out.println("Array2 has duplicates:"+hasDuplicates(arr2));
+
+
     }
 }
+
