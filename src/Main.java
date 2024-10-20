98595beef295fccc2f610e0fb780f5be401bbd60
sriharithavaka <sriharithavaka0401@gmail.com>
2024-10-19 23:16:53 -0700

    added duplicates method to main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.HashSet;
+
 public class Main {
+    public static boolean hasDuplicates(int[] arr){
+        //hasduplicates is a method where we give integers as input and gets boolean as output
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : arr){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
     public static void main(String[] args){
         System.out.println("Hello world");
         Sum sum1 = new Sum();
-        int[] arr1 = {6,7,8,9};
-        System.out.println("Array-x has sum:"+sum1.arraySum(arr1));
+        int[] arr3 = {6,7,8,9};
+        System.out.println("Array-x has sum:"+sum1.arraySum(arr3));
+        int[] arr1 = {1,2,3,2,4,5,1};
+        int[] arr2 = {1,2,3,4};
+        System.out.println("Array1 has duplicates:"+hasDuplicates(arr1));
+        System.out.println("Array2 has duplicates:"+hasDuplicates(arr2));
     }
 }
